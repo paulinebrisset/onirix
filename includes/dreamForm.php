3416ff59da28988245bb7d4625e1bc735919a439
Pauline Brisset <paulinebrisset@protonmail.com>
2023-05-16 16:19:45 +0400

    input

---
+++ includes/dreamForm.php
@@ -0,0 +1,11 @@
+<form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+    <?php $question = 'Racontez votre rêve à Onirix !'; ?>
+    <textarea name="dream" placeholder='<?php echo $question; ?>'></textarea>
+    <input type="submit">
+</form>
+
+<?php
+if (isset($_POST['dream'])) {
+    echo "<p>Contenu du rêve : " . $_POST['dream'] . "</p>";
+}
+?>
